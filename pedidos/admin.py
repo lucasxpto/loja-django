1036a5727ac93da72937ef2cc44030f085148c8c
Lucas Pedreira Vital <2021105100039@aluno.ifro.edu.br>
2023-05-31 22:09:13 -0400

    'aula'

---
+++ pedidos/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from pedidos.models import Endereco, Cliente, Pedido, Produto
+
+
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ['rua', 'numero', 'bairro']
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email', 'endereco']
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'data_pedido', 'valor']
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'valor']
+
+
